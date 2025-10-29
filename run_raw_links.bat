f6418109da65bf5001c60c7449440f787d832a18
tuguss11-source <tuguss11@gmail.com>
2025-10-29 09:04:35 +0300

    Add github_raw_links.py script

--- run_raw_links.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo GitHub Raw Links Generator Calistiriliyor...
-python github_raw_links.py tuguss11-source prototype0 --branch main --exclude-py-cache > raw_links.txt
-echo Islem tamamlandı! Sonuclar raw_links.txt dosyasina kaydedildi.
-pause
